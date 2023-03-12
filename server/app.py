fe4f689db3bcdb81c095e85a9fcc9a8c88067bda
Viral Rupani <viralrupani12017@gmail.com>
2023-03-12 14:48:32 +0530

    refactor: change database to MongoDB

--- server/app.py
+++ server/app.py
@@ -1,55 +1,41 @@
-from flask import Flask, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
+from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
-import random
 
-db = SQLAlchemy()
 app = Flask(__name__)
 
-# app configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///community.db'
-
-db.init_app(app=app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(20), nullable=False)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(250), nullable=False)
-
-
-with app.app_context():
-    db.create_all()
+client = MongoClient('localhost', 27017)
+db = client.chatDB
+users = db.users
 
 
 @app.route('/register', methods=["POST"])
 def register():
     json_data = request.get_json()
-    if User.query.filter_by(username=json_data["username"]).first():
+
+    if users.find_one({"username":json_data["username"] }):
         return {"response": "failur", "reason": "Username is already in use"}
     if len(json_data["password"]) < 8:
         return {"response": "failur", "reason": "Password is too short"}
-    new_user = User(
-        email=json_data["email"],
-        username=json_data["username"],
-        password=generate_password_hash(
+    new_user = {
+        "email": json_data["email"],
+        "username": json_data["username"],
+        "password": generate_password_hash(
             json_data["password"], method="pbkdf2:sha256", salt_length=8)
-    )
-    db.session.add(new_user)
-    db.session.commit()
+    }
+    users.insert_one(new_user)
     return {"response": "success"}, 200
 
 
 @app.route('/login', methods=["POST"])
 def login():
     json_data = request.get_json()
-    email = json_data["email"]
-    existing_user = User.query.filter_by(email=email).first()
+    username = json_data["username"]
+    existing_user = users.find_one({"username": username})
     if existing_user:
-        if check_password_hash(pwhash=existing_user.password, password=json_data["password"]):
+        if check_password_hash(pwhash=existing_user["password"], password=json_data["password"]):
             return {"response": "success"}
-    return {"response": "failur", "reson": "Email or Password issue"}
+    return {"response": "failur", "reason": "Username or Password issue"}
 
 
 @app.route('/')
